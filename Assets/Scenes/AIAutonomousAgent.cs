be1d352b255294ccce8e31769253e88bb89b89e5
PipoPleading <129329931+PipoPleading@users.noreply.github.com>
2024-01-09 14:32:50 -0700

    First Message
    Submitting first message 👍

---
+++ Assets/Scenes/AIAutonomousAgent.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIAutonomousAgent : AIAgent
+{
+    public AiPerception perception = null;
+
+    private void Update()
+    {
+        //
+    }
+
+}
---
+++ Assets/Scenes/AIDistancePerception.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIDistancePerception : AiPerception
+{
+    public override GameObject[] GetGameObjects()
+    {
+        throw new System.NotImplementedException();
+    }
+
+}
