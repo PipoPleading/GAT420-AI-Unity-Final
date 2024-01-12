6e588ab26fa57c39ac49c6f801fd92dee0a590cf
PipoPleading <129329931+PipoPleading@users.noreply.github.com>
2024-01-11 17:15:15 -0700

    Week 2
    All code and scene edits of week 2.

---
+++ Assets/Scripts/AIPerception.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AiPerception : MonoBehaviour
+{
+    [SerializeField] protected string tagName = "";
+    [SerializeField] protected float distance = 1;
+    [SerializeField] protected float maxAngle = 45;
+
+    public string TagName { get { return tagName; } }
+    public float Distance { get { return Distance; } }
+    public float MaxAngle { get { return maxAngle; } }
+
+    public abstract GameObject[] GetGameObjects();
+
+}
